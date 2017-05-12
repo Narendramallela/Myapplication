02296ba2d97311b78611906ed29fc60dd74db601
narendramallela <narendra1031@gmail.com>
2017-05-11 22:56:30 -0500

    Initial commit

--- mysite/canticle/urls.py
+++ mysite/canticle/urls.py
@@ -5,7 +5,6 @@ from django.contrib.auth import views as auth_views
 from .import views
 
 urlpatterns=[
-	url(r'^$',views.index,name='index'),
         url(r'^signup/$',views.signup,name='signup'),
         url(r'^login/$',views.login,name='login')
 ]
