1856355a9cc2322b7f7fa9d9d57b3473aa55edbd
narendramallela <narendra1031@gmail.com>
2017-06-10 16:17:37 -0500

    Intial commit

--- mysite/canticle/urls.py
+++ mysite/canticle/urls.py
@@ -6,5 +6,8 @@ from .import views
 
 urlpatterns=[
         url(r'^signup/$',views.signup,name='signup'),
-        url(r'^login/$',views.login,name='login')
+        url(r'^login/$',views.login,name='login'),
+        url(r'^logout/$', views.logout, name='logout'),
+        url(r'^login/info/$',views.info,name='info'),
+        
 ]
